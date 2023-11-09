9cfd5303944651ed4787ea715cb56701e086fd27
Ivan Shutov <vanasutov793@gmail.com>
2023-11-09 06:41:27 +0400

    ю

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const fileUpload = require('./custom_node_modules/JS/file_upload.js');
 
 let PORT = process.env.PORT ?? 80;
-let HOSTNAME = process.env.HOSTNAME ?? 'localhost';
+let HOSTNAME = process.env.HOSTNAME ?? '5.35.13.205';
 
 http
   .createServer((req, res) => {
